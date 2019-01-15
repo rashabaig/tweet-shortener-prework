9ac51aef9267a7426510a0f69b2dac6e4f489a3d
github username <rasha.baig@gmail.com>
2019-01-15 12:01:05 -0500

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def dictionary
+  words = {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter (tweet_string)
+array = tweet_string.split(" ")
+  array.each do |word|
+    if dictionary.keys include?(word)
+      word = key.value
+  array.join(", ")
+end
+end
+end
+
+def bulk_tweet_shortener(tweets_array)
+  puts word_substituter
+end
+
+def shortened_tweet_truncator(tweet)
+  puts "#{tweet}[0..140]..."
+end
